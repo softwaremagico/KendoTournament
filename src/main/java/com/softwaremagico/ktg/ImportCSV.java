759098e89110fb0d941fb17ac7bd5d1814a87351
jhortelano <jhortelano@VirtualUbuntu-O>
2012-07-17 14:15:38 +0200

    Automated Licensing

--- src/main/java/com/softwaremagico/ktg/ImportCSV.java
+++ src/main/java/com/softwaremagico/ktg/ImportCSV.java
@@ -1,22 +1,30 @@
+package com.softwaremagico.ktg;
 /*
- *  This software is designed by Jorge Hortelano Otero.
+ * #%L
+ * KendoTournamentGenerator
+ * %%
+ * Copyright (C) 2008 - 2012 Softwaremagico
+ * %%
+ * This software is designed by Jorge Hortelano Otero.
  *  softwaremagico@gmail.com
  *  Copyright (C) 2012 Jorge Hortelano Otero.
  *  C/Quart 89, 3. Valencia CP:46008 (Spain).
+ *  
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
  *  as published by the Free Software Foundation; either version 2
  *  of the License, or (at your option) any later version.
+ *  
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
+ *  
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *  Created on 28-ene-2011.
+ *  along with this program; If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
  */
-package com.softwaremagico.ktg;
 
 import com.softwaremagico.ktg.files.MyFile;
 import com.softwaremagico.ktg.language.LanguagePool;
