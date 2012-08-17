21f50fc3b08ea8d684f282c495e7ffb7219ba0fa
jorge <jorge@Elmara>
2012-08-17 16:26:51 +0200

    Nada más...

--- src/main/java/com/softwaremagico/ktg/tournament/BlackBoardPanel.java
+++ src/main/java/com/softwaremagico/ktg/tournament/BlackBoardPanel.java
@@ -1,26 +1,21 @@
 package com.softwaremagico.ktg.tournament;
 /*
- * #%L
- * KendoTournamentGenerator
- * %%
- * Copyright (C) 2008 - 2012 Softwaremagico
- * %%
+ * #%L KendoTournamentGenerator %% Copyright (C) 2008 - 2012 Softwaremagico %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> C/Quart 89, 3. Valencia CP:46008 (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
- * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
+ * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>. #L%
  */
 
 import com.softwaremagico.ktg.Fight;
@@ -122,36 +117,35 @@ public class BlackBoardPanel extends javax.swing.JPanel {
         /*
          * Paint teams group
          */
-        try{
-        for (int level = 0; level < KendoTournamentGenerator.getInstance().tournamentManager.getLevels().size(); level++) {
-            for (int groupIndex = 0; groupIndex < KendoTournamentGenerator.getInstance().tournamentManager.getLevels().get(level).getGroups().size(); groupIndex++) {
-                try {
-                    if (KendoTournamentGenerator.getInstance().tournamentManager.getLevels().get(level).getGroups().get(groupIndex).tournament.equals(tournament)) {
-                        c.anchor = GridBagConstraints.WEST;
-                        c.gridx = level * 2 + 1 + titleColumn;
-                        if (KendoTournamentGenerator.getInstance().tournamentManager.default_max_winners < 2) {
-                            c.gridy = groupIndex * (int) (Math.pow(2, level)) + titleRow;
-                        } else {
-                            if (level == 0) {
+        try {
+            for (int level = 0; level < KendoTournamentGenerator.getInstance().tournamentManager.getLevels().size(); level++) {
+                for (int groupIndex = 0; groupIndex < KendoTournamentGenerator.getInstance().tournamentManager.getLevels().get(level).getGroups().size(); groupIndex++) {
+                    try {
+                        if (KendoTournamentGenerator.getInstance().tournamentManager.getLevels().get(level).getGroups().get(groupIndex).tournament.equals(tournament)) {
+                            c.anchor = GridBagConstraints.WEST;
+                            c.gridx = level * 2 + 1 + titleColumn;
+                            if (KendoTournamentGenerator.getInstance().tournamentManager.default_max_winners < 2) {
                                 c.gridy = groupIndex * (int) (Math.pow(2, level)) + titleRow;
                             } else {
-                                c.gridy = groupIndex * (int) (Math.pow(2, level - 1)) + titleRow;
+                                if (level == 0) {
+                                    c.gridy = groupIndex * (int) (Math.pow(2, level)) + titleRow;
+                                } else {
+                                    c.gridy = groupIndex * (int) (Math.pow(2, level - 1)) + titleRow;
+                                }
+                            }
+
+                            c.weightx = 0.5;
+                            add(KendoTournamentGenerator.getInstance().tournamentManager.getLevels().get(level).getGroups().get(groupIndex), c);
+                            if (level == 0 && groupIndex == 0) {
+                                KendoTournamentGenerator.getInstance().tournamentManager.getLevels().get(level).getGroups().get(groupIndex).setSelected(KendoTournamentGenerator.getInstance().tournamentManager);
                             }
                         }
-
-                        c.weightx = 0.5;
-                        add(KendoTournamentGenerator.getInstance().tournamentManager.getLevels().get(level).getGroups().get(groupIndex), c);
-                        if (level == 0 && groupIndex == 0) {
-                            KendoTournamentGenerator.getInstance().tournamentManager.getLevels().get(level).getGroups().get(groupIndex).setSelected(KendoTournamentGenerator.getInstance().tournamentManager);
-                        }
+                    } catch (NullPointerException npe) {
+                        KendoTournamentGenerator.getInstance().showErrorInformation(npe);
                     }
-                } catch (NullPointerException npe) {
-                    KendoTournamentGenerator.getInstance().showErrorInformation(npe);
                 }
             }
-        }
-        }catch(ClassCastException e){
-            
+        } catch (ClassCastException e) {
         }
     }
 
@@ -185,7 +179,7 @@ public class BlackBoardPanel extends javax.swing.JPanel {
                 }
             }
         } catch (Exception e) {
-           KendoTournamentGenerator.getInstance().showErrorInformation(e);
+            KendoTournamentGenerator.getInstance().showErrorInformation(e);
         }
     }
 
--- src/main/java/com/softwaremagico/ktg/tournament/LeagueDesigner.java
+++ src/main/java/com/softwaremagico/ktg/tournament/LeagueDesigner.java
@@ -1,26 +1,21 @@
 package com.softwaremagico.ktg.tournament;
 /*
- * #%L
- * KendoTournamentGenerator
- * %%
- * Copyright (C) 2008 - 2012 Softwaremagico
- * %%
+ * #%L KendoTournamentGenerator %% Copyright (C) 2008 - 2012 Softwaremagico %%
  * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
  * <softwaremagico@gmail.com> C/Quart 89, 3. Valencia CP:46008 (Spain).
- *  
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- *  
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *  
+ *
  * You should have received a copy of the GNU General Public License along with
- * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
+ * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>. #L%
  */
 
 import com.softwaremagico.ktg.*;
@@ -261,7 +256,7 @@ public class LeagueDesigner extends javax.swing.JFrame {
 
     private void updateInterface() {
         try {
-            tournament = (Tournament)(TournamentComboBox.getSelectedItem());
+            tournament = (Tournament) (TournamentComboBox.getSelectedItem());
             KendoTournamentGenerator.getInstance().tournamentManager = new TournamentGroupManager(tournament);
             teams = KendoTournamentGenerator.getInstance().database.searchTeamsByTournamentExactName(tournament, false);
 
@@ -284,7 +279,7 @@ public class LeagueDesigner extends javax.swing.JFrame {
         try {
             Integer select = KendoTournamentGenerator.getInstance().tournamentManager.getIndexLastSelected();
             if (!KendoTournamentGenerator.getInstance().tournamentManager.getMode().equals(TournamentTypes.SIMPLE)) {
-                bbp.updateBlackBoard((Tournament)TournamentComboBox.getSelectedItem(), false);
+                bbp.updateBlackBoard((Tournament) TournamentComboBox.getSelectedItem(), false);
                 KendoTournamentGenerator.getInstance().tournamentManager.enhance(false);
             } else {
                 bbp.clearBlackBoard();
