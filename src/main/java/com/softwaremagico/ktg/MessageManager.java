4b2ff492e0c8eb3e231d61a8b556a94ee33999d9
jhortelano <jhortelano@VirtualUbuntu-O>
2012-08-23 12:32:58 +0200

    Errors are shown in debug mode.

--- src/main/java/com/softwaremagico/ktg/MessageManager.java
+++ src/main/java/com/softwaremagico/ktg/MessageManager.java
@@ -1,32 +1,28 @@
 package com.softwaremagico.ktg;
 /*
- * #%L
- * KendoTournamentGenerator
- * %%
- * Copyright (C) 2008 - 2012 Softwaremagico
- * %%
- * This software is designed by Jorge Hortelano Otero.
- * Jorge Hortelano Otero <softwaremagico@gmail.com>
- * C/Quart 89, 3. Valencia CP:46008 (Spain).
- *  
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *  
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *  
- * You should have received a copy of the GNU General Public License
- * along with this program; If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
+ * #%L KendoTournamentGenerator %% Copyright (C) 2008 - 2012 Softwaremagico %%
+ * This software is designed by Jorge Hortelano Otero. Jorge Hortelano Otero
+ * <softwaremagico@gmail.com> C/Quart 89, 3. Valencia CP:46008 (Spain).
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/gpl-3.0.html>. #L%
  */
 
 import com.softwaremagico.ktg.language.LanguagePool;
 import com.softwaremagico.ktg.language.Translator;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -94,6 +90,12 @@ public class MessageManager {
         showGraphicMessage(text, title, JOptionPane.ERROR_MESSAGE);
     }
 
+    public static void errorMessage(Throwable throwable) {
+        String error = getStackTrace(throwable);
+        basicErrorMessage(error, "Error");
+        throwable.printStackTrace();
+    }
+
     public static void customMessage(String text, String title, int option) {
         showGraphicMessage(text, title, option);
         Log.finest(title + ": " + text);
@@ -141,5 +143,12 @@ public class MessageManager {
         } else {
             return false;
         }
+    }
+
+    private static String getStackTrace(Throwable throwable) {
+        Writer writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        throwable.printStackTrace(printWriter);
+        return writer.toString();
     }
 }
