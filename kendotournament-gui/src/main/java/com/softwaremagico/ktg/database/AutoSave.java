18e148ac39a7993d6915daed6acbfd7787b9264b
jorge <jorge@Elmara>
2013-04-11 17:48:19 +0200

    Autosave panel deleted.

--- kendotournament-gui/src/main/java/com/softwaremagico/ktg/database/AutoSave.java
+++ kendotournament-gui/src/main/java/com/softwaremagico/ktg/database/AutoSave.java
@@ -2,15 +2,8 @@ package com.softwaremagico.ktg.database;
 
 import com.softwaremagico.ktg.core.KendoLog;
 import com.softwaremagico.ktg.core.KendoTournamentGenerator;
-import com.softwaremagico.ktg.gui.base.KFrame;
-import com.softwaremagico.ktg.language.LanguagePool;
-import com.softwaremagico.ktg.language.Translator;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.util.Timer;
 import java.util.TimerTask;
-import javax.swing.JLabel;
 
 public class AutoSave {
 
@@ -18,15 +11,10 @@ public class AutoSave {
     private static AutoSave instance = new AutoSave(AUTOSAVE_PERIOD);
     private Timer timer = new Timer("Autosave");
     private Task timerTask;
-    private static Translator trans;
-    //private AutosavePanel autosavePanel;
 
     private AutoSave(Integer autosaveTime) {
-        trans = LanguagePool.getTranslator("messages.xml");
-        //autosavePanel = new AutosavePanel();
         timerTask = new Task();
         timer.schedule(timerTask, 0, 1000 * autosaveTime);
-        // autosavePanel.setVisible(true);
 
     }
 
@@ -42,50 +30,12 @@ public class AutoSave {
 
         @Override
         public void run() {
-            // autosavePanel.setVisible(true);
-            // autosavePanel.setAlwaysOnTop(true);
             if (KendoTournamentGenerator.isAutosaveOptionSelected()) {
                 KendoLog.debug(AutoSave.class.getName(), "Autosaving...");
                 if (DatabaseConnection.getInstance().isDatabaseConnectionTested()) {
                     DatabaseConnection.getInstance().updateDatabase();
                 }
             }
-            // autosavePanel.setVisible(false);
-            // autosavePanel.dispose();
-        }
-    }
-
-    class AutosavePanel extends KFrame {
-
-        public AutosavePanel() {
-            defineWindow(300, 180);
-            setResizable(false);
-            setElements();
-        }
-
-        private void setElements() {
-            setLayout(new GridBagLayout());
-            GridBagConstraints gridBagConstraints = new GridBagConstraints();
-            String text = trans.getTranslatedText("autosaving");
-            JLabel label = new JLabel(text);
-            gridBagConstraints.fill = GridBagConstraints.BOTH;
-            gridBagConstraints.ipadx = xPadding;
-            gridBagConstraints.gridx = 0;
-            gridBagConstraints.gridy = 0;
-            gridBagConstraints.gridheight = 1;
-            gridBagConstraints.gridwidth = 1;
-            gridBagConstraints.weightx = 1;
-            gridBagConstraints.weighty = 1;
-            gridBagConstraints.insets = new Insets(5, 5, 5, 5);
-            getContentPane().add(label, gridBagConstraints);
-        }
-
-        @Override
-        public void update() {
-        }
-
-        @Override
-        public void tournamentChanged() {
         }
     }
 }
